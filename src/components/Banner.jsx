023f44462137a12245d8947b19c9b5d56f43a5ab
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-06-24 00:59:24 -0400

    load the banner on the homepage

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default Banner = () => {
+export default function Banner () {
     return (
         <div className="banner">
         <h1>New Collection</h1>
         <p>shorts, tees, tanks & more!</p>
-        <button className='bg-green'>Shop Now</button>
+        <button>Shop Now</button>
         </div>
     );
     }
