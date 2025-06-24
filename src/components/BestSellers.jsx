aa21315ef1585cb03b3e326616815a764a296988
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-06-24 02:44:28 -0400

    add padding to the side of gallery

--- src/components/BestSellers.jsx
+++ src/components/BestSellers.jsx
@@ -3,45 +3,45 @@ import ProductInfo from "./ProductInfo";
 
 export default function BestSellers() {
     return (
-        <div>
+        <div className="py-10">
         <div className="new-arrivals d-flex flex-column align-items-center p-4">
         <h2 className="fs-3 fw-bold mb-4">best sellers</h2>
         <h3 className="fs-6">The hottest trends in fashion today</h3>
         </div>
-        <div className="d-flex flex-row">
-            <div className="d-flex flex-column align-items-start  p-2">
-            <img src="./shorts.png" alt="New Arrival 1" className="img-fluid p-2 w-25" />
-            <ProductInfo />
-            </div>
 
+     <div className="row px-5">
+  <div className="col-6 col-md-3 mb-4">
+          <img src="./shorts.png" alt="New Arrival 1" className="img-fluid p-2" />
+          <ProductInfo />
+        </div>
 
-   <div className="d-flex flex-column align-items-start  p-2">
-            <img src="./shorts.png" alt="New Arrival 1" className="img-fluid p-2 w-25" />
+  <div className="col-6 col-md-3 mb-4">
+            <img src="./shorts.png" alt="New Arrival 1" className="img-fluid p-2" />
             <ProductInfo />
             </div>
                         
-   <div className="d-flex flex-column align-items-start p-2">
-            <img src="./shorts.png" alt="New Arrival 1" className="img-fluid p-2 w-25" />
+  <div className="col-6 col-md-3 mb-4">
+            <img src="./shorts.png" alt="New Arrival 1" className="img-fluid p-2" />
             <ProductInfo />
             </div>
-            <div className="d-flex flex-column align-items-start  p-2">
-            <img src="./shorts.png" alt="New Arrival 1" className="img-fluid p-2 w-25" />
+  <div className="col-6 col-md-3 mb-4">
+            <img src="./shorts.png" alt="New Arrival 1" className="img-fluid p-2" />
             <ProductInfo />
             </div>
-               <div className="d-flex flex-column align-items-start p-2">
-            <img src="./shorts.png" alt="New Arrival 1" className="img-fluid p-2 w-25" />
+  <div className="col-6 col-md-3 mb-4">
+            <img src="./shorts.png" alt="New Arrival 1" className="img-fluid p-2" />
             <ProductInfo />
             </div>
-        <div className="d-flex flex-column align-items-start p-2">
-            <img src="./shorts.png" alt="New Arrival 1" className="img-fluid p-2 w-25" />
+  <div className="col-6 col-md-3 mb-4">
+            <img src="./shorts.png" alt="New Arrival 1" className="img-fluid p-2" />
             <ProductInfo />
             </div>
-               <div className="d-flex flex-column align-items-start p-2">
-            <img src="./shorts.png" alt="New Arrival 1" className="img-fluid p-2 w-25" />
+  <div className="col-6 col-md-3 mb-4">
+            <img src="./shorts.png" alt="New Arrival 1" className="img-fluid p-2" />
             <ProductInfo />
             </div>
-               <div className="d-flex flex-column align-items-start p-2">
-            <img src="./shorts.png" alt="New Arrival 1" className="img-fluid p-2 w-25" />
+  <div className="col-6 col-md-3 mb-4">
+            <img src="./shorts.png" alt="New Arrival 1" className="img-fluid p-2" />
             <ProductInfo />
             </div>
             </div>
--- src/components/ProductInfo.jsx
+++ src/components/ProductInfo.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function ProductInfo() {
     return (
-        <div className="product-info d-flex flex-column align-items-start justify-content-center">
-            <h4 className="fs-6 fw-bold">Product Title</h4>
+        <div className="product-info d-flex flex-column align-items-start justify-content-center mx-2">
+            <h4 className="fs-6 fw-bold">product title</h4>
             <p className="fs-6 text-center"> $0.00
             </p>
       
