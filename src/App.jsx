ce2549ccdb883fbcba4f84bda96490a3d9bdaa72
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-06-24 00:35:19 -0400

    add dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,19 @@ import {
 } from "react-router-dom";
 import Root from "./Root.jsx";
 import "./App.css";
+import Banner from "./components/Banner.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />}>
+      <Route index element={<Banner />} /> 
     </Route>
   )
 );
 
 function App() {
   return <RouterProvider router={router} />;
+
 }
 
 export default App;
