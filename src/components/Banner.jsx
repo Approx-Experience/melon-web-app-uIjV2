9fdf87166e68ad16b07b9a73bc5f19dc76ad1fdb
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-06-24 01:17:38 -0400

    utilize Bootstrap to style the button and fonts

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 export default function Banner () {
     return (
         <div className="banner">
-        <h1>New Collection</h1>
-        <p>shorts, tees, tanks & more!</p>
-        <button>Shop Now</button>
+            <h1 className='fs-6'>SUMMER 2025</h1>
+        <h2 className='fs-1 fw-bold'>New Collection</h2>
+        <p className='fs-6'>shorts, tees, tanks & more!</p>
+        <Button 
+        style={{ backgroundColor: "#35e15c", color: "black" }}  size="md" variant="outline-light" 
+         className="px-4 py-2 fw-bold">
+            SHOP NOW
+        </Button> 
         </div>
     );
     }
