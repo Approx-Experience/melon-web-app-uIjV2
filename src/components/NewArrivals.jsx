24d3e0163864ec0a1626d5685cbfe439d6c3ef7b
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-06-24 02:56:07 -0400

    new arrivals grid complete

--- src/components/NewArrivals.jsx
+++ src/components/NewArrivals.jsx
@@ -5,11 +5,16 @@ export default function NewArrivals() {
         <div className="new-arrivals d-flex flex-column align-items-center justify-content-center p-4">
         <h2 className="fs-3 fw-bold mb-4">new arrivals</h2>
         <h3 className="fs-6">Fashionable threads for your eyes to see</h3>
-        <div className="d-flex flex-wrap justify-content-center">
-            <img src="./shirts.png" alt="New Arrival 1" className="img-fluid m-2 w-25" />
-            <img src="./shorts.png" alt="New Arrival 2" className="img-fluid m-2 w-25" />
-            <img src="./hats.png" alt="New Arrival 3" className="img-fluid m-2 w-25" />
-            <img src="./athleisure.png" alt="New Arrival 4" className="img-fluid m-2 w-25" />
+        <div className="d-flex">
+           <div className="d-flex flex-wrap justify-content-center">
+            <img src="./shirts.png" alt="New Arrival 1" className="img-fluid m-2" style={{ height: "416px", width: "auto" }} />
+            <img src="./shorts.png" alt="New Arrival 2" className="img-fluid m-2" style={{ height: "416px", width: "auto" }}/>
+            </div>
+
+            <div className="d-flex flex-column flex-wrap">
+            <img src="./hats.png" alt="New Arrival 3" className="img-fluid m-2 " style={{ height: "200px", width: "auto" }}/>
+            <img src="./athleisure.png" alt="New Arrival 4" className="img-fluid m-2" style={{ height: "200px", width: "auto" }} />
+            </div>
         </div>
         </div>
     );
