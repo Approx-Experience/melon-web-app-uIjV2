79e83cfc9dea3fe0dfe58429e99dd67e97771764
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-06-26 16:36:58 -0400

    work on nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,29 +16,51 @@
 export default function NavBar() {
     return (
  <div>
-    <nav className="navbar d-flex flex-column p-3"
+    <nav className="navbar d-flex flex-column pt-5 px-4"
     style={{ backgroundColor: "#FFECDA" }}>
     
-    <div className="d-flex flex-row gap-4">
-        
-        <h1 className="fw-bold"
+    <div className="d-flex flex-row justify-content-between align-items-center mb-3 w-100">
+    
+    <div className="d-flex flex-row gap-2">    
+        <h1 className="fw-bold fs-4"
         style={{color: "#E13636"}}>melon</h1>
         <img src="./melon-icon.svg" alt="Melon Logo" className="logo" />
         </div>
 
-        <div>
-        <input className="w-100 px-5"
+
+        <div className="position-relative">
+        <img src="./search.svg"
+        className="position-absolute start-0 top-50 translate-middle-y"
+        alt="Search" style={{ width: "20px", height: "20px" }} />
+
+        <input className="px-5 py-1 rounded-1 border-1"
+
         placeholder="Find your next fit"
-        ></input>
+        style={{ backgroundColor: "#FFECDA", 
+            width: "550px",
+        }}
+        >
+        </input>
     </div>
 
-        <div>
-            <p> account </p>
-            <p> favorites</p>
-            <p>cart</p>
-        </div>
 
-        <div className="d-flex justify-content-between w-50">
+
+
+
+
+
+
+
+        <div className="d-flex flex-row gap-3 fw-bold font-14">
+            <div> <img src="./account.svg"></img> account </div>
+            <div> <img src="./favorite.svg"></img> favorites</div>
+            <div className="fw-normal"> <img src="./cart.svg"></img> 0</div>
+        </div>
+</div>
+
+
+        <div className="d-flex justify-content-between fw-bold font-14"
+        style={{width: "500px"}}>
         <p>new arrivals</p>
          <p>men</p>
           <p>women</p>
