f27fe031ddda0fe462609e0c09797f39c3113993
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-06-26 15:37:46 -0400

    work on nav bar styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,12 +16,36 @@
 export default function NavBar() {
     return (
  <div>
-    <nav className="navbar d-flex justify-content-between align-items-center p-3"
+    <nav className="navbar d-flex flex-column p-3"
     style={{ backgroundColor: "#FFECDA" }}>
     
+    <div className="d-flex flex-row gap-4">
+        
         <h1 className="fw-bold"
         style={{color: "#E13636"}}>melon</h1>
         <img src="./melon-icon.svg" alt="Melon Logo" className="logo" />
+        </div>
+
+        <div>
+        <input className="w-100 px-5"
+        placeholder="Find your next fit"
+        ></input>
+    </div>
+
+        <div>
+            <p> account </p>
+            <p> favorites</p>
+            <p>cart</p>
+        </div>
+
+        <div className="d-flex justify-content-between w-50">
+        <p>new arrivals</p>
+         <p>men</p>
+          <p>women</p>
+           <p>accessories</p>
+            <p>shoes</p>
+             <p>sales</p>
+             </div>
     </nav>
 
  </div> 
