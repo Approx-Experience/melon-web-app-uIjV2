ac6f4cd609e06ed03f0a1ce87e518ba351165f96
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-06-26 22:45:29 -0400

    fix spacing for nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -17,12 +17,13 @@ export default function NavBar() {
     return (
  <div>
     <nav className="navbar d-flex flex-column pt-5 px-4"
+
     style={{ backgroundColor: "#FFECDA" }}>
     
     <div className="d-flex flex-row justify-content-between align-items-center mb-3 w-100">
     
-    <div className="d-flex flex-row gap-2">    
-        <h1 className="fw-bold fs-4"
+    <div className="d-flex flex-row  gap-2">    
+        <h1 className="fw-bold fs-4 mx-4"
         style={{color: "#E13636"}}>melon</h1>
         <img src="./melon-icon.svg" alt="Melon Logo" className="logo" />
         </div>
@@ -34,21 +35,18 @@ export default function NavBar() {
         alt="Search" style={{ width: "20px", height: "20px" }} />
 
         <input className="px-5 py-1 rounded-1 border-1"
-
+        type="text"
+        id="search"
+        name="search"
+        aria-label="Search"
         placeholder="Find your next fit"
         style={{ backgroundColor: "#FFECDA", 
             width: "550px",
+            fontSize: "13px",
         }}
         >
         </input>
     </div>
-
-
-
-
-
-
-
 
 
         <div className="d-flex flex-row gap-3 fw-bold font-14">
@@ -59,14 +57,14 @@ export default function NavBar() {
 </div>
 
 
-        <div className="d-flex justify-content-between fw-bold font-14"
+        <div className="d-flex justify-content-around fw-bold font-14"
         style={{width: "500px"}}>
         <p>new arrivals</p>
          <p>men</p>
           <p>women</p>
            <p>accessories</p>
             <p>shoes</p>
-             <p>sales</p>
+             <p>sale</p>
              </div>
     </nav>
 
