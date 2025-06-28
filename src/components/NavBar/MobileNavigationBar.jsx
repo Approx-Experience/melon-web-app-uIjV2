69803e7623624fb6d58d0c914360e7706bcc7a70
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-06-28 17:38:48 -0400

    remove inline style for mobile nav

--- src/components/NavBar/MobileNavigationBar.jsx
+++ src/components/NavBar/MobileNavigationBar.jsx
@@ -1,27 +1,23 @@
+import "./MobileNavigationBar.css";
+
 export default function NavBar() {
   return (
     <div>
-      <nav
-        className="navbar d-flex flex-column pt-3 px-4"
-        style={{ backgroundColor: "#FFECDA" }}
-      >
+      <nav className="navbar d-flex flex-column pt-3 px-4 background-color">
         <div className="d-flex flex-row justify-content-between align-items-center w-100">
           <div className="d-flex flex-column">
-            <h1 className="fw-bold fs-6" style={{ color: "#E13636" }}>
-              melon
-            </h1>
+            <h1 className="fw-bold fs-6 text-color">melon</h1>
             <a href="/">
               <img src="./melon-icon.svg" alt="Melon Logo" className="logo" />
             </a>
           </div>
 
           <div>
-            <div className="d-flex flex-row  fw-bold font-14 align-items-center mx-2 gap-4"> 
+            <div className="d-flex flex-row  fw-bold font-14 align-items-center mx- gap-4">
               <img
                 src="./search.svg"
-                className=""
+                className="search-icon"
                 alt="Search icon"
-                style={{ width: "20px", height: "20px" }}
               />
 
               <div>
@@ -40,8 +36,6 @@ export default function NavBar() {
                 <img src="./menu.svg" alt="Menu icon"></img>
               </div>
             </div>
-
-
           </div>
         </div>
       </nav>
