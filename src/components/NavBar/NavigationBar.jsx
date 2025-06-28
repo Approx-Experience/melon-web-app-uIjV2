347a9300f9d57adc51a6efd9cfbdedc595740444
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-06-28 17:49:38 -0400

    remove inline style for desktop nav

--- src/components/NavBar/NavigationBar.jsx
+++ src/components/NavBar/NavigationBar.jsx
@@ -1,13 +1,14 @@
+import "./NavigationBar.css";
+
 export default function NavBar() {
   return (
     <div>
       <nav
-        className="navbar d-flex flex-column pt-5 px-4"
-        style={{ backgroundColor: "#FFECDA" }}
+        className="navbar d-flex flex-column pt-5 px-4 background-color"
       >
         <div className="d-flex flex-row justify-content-between align-items-center mb-3 w-100">
           <div className="d-flex flex-row  gap-2">
-            <h1 className="fw-bold fs-4 mx-2" style={{ color: "#E13636" }}>
+            <h1 className="fw-bold fs-4 mx-2 text-color">
               melon
             </h1>
             <a href="/">
@@ -18,24 +19,17 @@ export default function NavBar() {
           <div className="position-relative">
             <img
               src="./search.svg"
-              className="position-absolute start-0 top-50 translate-middle-y"
+              className="position-absolute start-0 top-50 translate-middle-y search-icon"
               alt="Search icon"
-              style={{ width: "20px", height: "20px" }}
             />
 
             <input
-              className="px-5 py-1 rounded-1 border-1"
+              className="px-5 py-1 rounded-1 border-1 input-style"
               type="text"
               id="search"
               name="search"
               aria-label="Search"
-              placeholder="Find your next fit"
-              style={{
-                backgroundColor: "#FFECDA",
-                width: "550px",
-                fontSize: "13px",
-              }}
-            ></input>
+              placeholder="Find your next fit"></input>
           </div>
 
           <div className="d-flex flex-row gap-3 fw-bold font-14">
@@ -55,8 +49,7 @@ export default function NavBar() {
         </div>
 
         <div
-          className="d-flex justify-content-around fw-bold font-14"
-          style={{ width: "500px" }}
+          className="d-flex justify-content-around fw-bold font-1 menu-items-style"
         >
           <p>new arrivals</p>
           <p>men</p>
