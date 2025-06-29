5778fbf365b7e5a4f38340bc8f5d1830ee913682
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-06-29 13:33:52 -0400

    remove mobile text as placeholder

--- src/components/NewArrivals/NewArrivalsMobile.jsx
+++ src/components/NewArrivals/NewArrivalsMobile.jsx
@@ -5,7 +5,7 @@ import "./NewArrivalsMobile.css";
 export default function NewArrivals() {
   return (
     <div className="new-arrivals d-flex flex-column align-items-center justify-content-center">
-      <h2 className="fs-3 fw-bold m-4">new arrivals MOBILE</h2>
+      <h2 className="fs-3 fw-bold m-4">new arrivals</h2>
       <h3 className="font-14">Fashionable threads for your eyes to see</h3>
       <div className="d-flex">
         <div className="d-flex flex-wrap justify-content-center new-arrivals-photos">
