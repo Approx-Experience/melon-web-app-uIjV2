17fb005ebc0109caca678dc15a1fab8971f27d0f
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-06-30 12:28:39 -0400

    add product and iterate through data to display

---
+++ src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default Banner = () => {
+    return (
+        <div className="banner">
+        <h1>New Collection</h1>
+        <p>shorts, tees, tanks & more!</p>
+        <button className='bg-green'>Shop Now</button>
+        </div>
+    );
+    }
--- src/components/BestSellers/BestSellers.jsx
+++ src/components/BestSellers/BestSellers.jsx
@@ -1,6 +1,3 @@
-import React from "react";
-import ProductInfo from "../ProductInfo/ProductInfo";
-
 export default function BestSellers() {
   return (
     <div className="">
@@ -8,77 +5,9 @@ export default function BestSellers() {
         <h2 className="fs-3 fw-bold mb-4">best sellers</h2>
         <h3 className="font-14">The hottest trends in fashion today</h3>
       </div>
-<div className="container-fluid">
-      <div className="row">
-        <div className="col-6 col-md-3 mb-4">
-          <img
-            src="./shorts.png"
-            alt="New Arrival 1"
-            className="img-fluid"
-          />
-          <ProductInfo />
-        </div>
-
-        <div className="col-6 col-md-3 mb-4">
-          <img
-            src="./shorts.png"
-            alt="New Arrival 1"
-            className="img-fluid"
-          />
-          <ProductInfo />
-        </div>
-
-        <div className="col-6 col-md-3 mb-4">
-          <img
-            src="./shorts.png"
-            alt="New Arrival 1"
-            className="img-fluid"
-          />
-          <ProductInfo />
-        </div>
-        <div className="col-6 col-md-3 mb-4">
-          <img
-            src="./shorts.png"
-            alt="New Arrival 1"
-            className="img-fluid"
-          />
-          <ProductInfo />
-        </div>
-        <div className="col-6 col-md-3 mb-4">
-          <img
-            src="./shorts.png"
-            alt="New Arrival 1"
-            className="img-fluid"
-          />
-          <ProductInfo />
-        </div>
-        <div className="col-6 col-md-3 mb-4">
-          <img
-            src="./shorts.png"
-            alt="New Arrival 1"
-            className="img-fluid"
-          />
-          <ProductInfo />
-        </div>
-        <div className="col-6 col-md-3 mb-4">
-          <img
-            src="./shorts.png"
-            alt="New Arrival 1"
-            className="img-fluid"
-          />
-          <ProductInfo />
-        </div>
-        <div className="col-6 col-md-3 mb-4">
-          <img
-            src="./shorts.png"
-            alt="New Arrival 1"
-            className="img-fluid"
-          />
-          <ProductInfo />
-        </div>
+      <div className="container-fluid">
+        <div className="row"></div>
       </div>
-</div>
-
     </div>
   );
 }
