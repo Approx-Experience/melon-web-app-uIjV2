31b29474b066b799210f186cc6314509124f9d06
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-07-01 11:38:08 -0400

    add browse link to button, change to primary variant, and remove duplicate classes

--- src/components/Banner/MobileBanner.jsx
+++ src/components/Banner/MobileBanner.jsx
@@ -3,7 +3,7 @@ import "./MobileBanner.css";
 
 export default function Banner() {
   return (
-    <div className="banner bannerimage d-flex align-items-center justify-content-around align-items-center p-5">
+    <div className="banner bannerimage d-flex align-items-center justify-content-around p-5">
       <div className="d-flex flex-column align-items-center banner-content">
         <h1 className="fs-6 mb-4 summerMobile textMobile">SUMMER 2025</h1>
         <h2 className="bannerMobileTitle textMobile">
@@ -11,9 +11,9 @@ export default function Banner() {
           COLLECTION
         </h2>
         <p className="fs-6 pb-5 mb-5 textMobile">shorts, tees, tanks & more!</p>
-        <Button variant="none" className="mobile-banner-button">
+        <a href="/browse"><Button variant="primary" className="mobile-banner-button">
           SHOP NOW
-        </Button>
+        </Button></a>
       </div>
     </div>
   );
